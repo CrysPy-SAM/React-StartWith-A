bb6b5366b49aea1d3b421a66de663b28fbccdf99
CrysPy-SAM <ssmishra9918@gmail.com>
2025-09-06 15:56:33 +0530

    App.js

--- Projects/Todo-app-version-one/src/App.jsx
+++ Projects/Todo-app-version-one/src/App.jsx
@@ -1,17 +1,46 @@
-import './App.css'
-
 function App() {
-  return <div>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-light">Light</button>
-    <button type="button" class="btn btn-dark">Dark</button>
-    <button type="button" class="btn btn-link">Link</button>
-  </div>
+  return (
+    <center className="todo-container">
+      <h1>TODO App</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter Todo Here" />
+          </div>
+          <div className="col-4">
+            <input type="date" />
+          </div>
+          <div className="col-2">
+            <button type="button" className="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+
+        {/* First row item */}
+        <div className="row">
+          <div className="col-6">Buy Milk</div>
+          <div className="col-4">4/10/2023</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        {/* Second row item */}
+        <div className="row">
+          <div className="col-6">Go to College</div>
+          <div className="col-4">4/10/2023</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default App;
