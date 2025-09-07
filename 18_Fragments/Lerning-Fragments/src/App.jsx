900045e5b88c1a0dfe97da00a98cd5bdc5dabe85
CrysPy-SAM <ssmishra9918@gmail.com>
2025-09-07 11:27:13 +0530

    map()

--- 18_Fragments/Lerning-Fragments/src/App.jsx
+++ 18_Fragments/Lerning-Fragments/src/App.jsx
@@ -2,17 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
+    
+  let foodItems =  ['Dal', 'Fruits', 'Green Veg', 'Milk', 'Juices', 'Dry Fruits'];
+
 return  (
-  <>
+  <center>
+    <>
        <h1>Healthy Food</h1>
-  <ul class="list-group">
-  <li class="list-group-item">Fruits</li>
-  <li class="list-group-item">Daal</li>
-  <li class="list-group-item">Green Veg</li>
-  <li class="list-group-item">Milk</li>
-  <li class="list-group-item">Juices</li>
+  <ul className="list-group">
+  {foodItems.map((item) =>(
+    <li key={item} className="list-group-item">{item}</li>
+  ))}
 </ul>
-  </>
+  </> 
+  </center>
 )
 }
 
