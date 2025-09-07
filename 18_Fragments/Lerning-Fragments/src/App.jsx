78ea540edd0d8045757395a7f7690a6b48f75de7
CrysPy-SAM <ssmishra9918@gmail.com>
2025-09-07 11:45:54 +0530

    Conditional Rendering

--- 18_Fragments/Lerning-Fragments/src/App.jsx
+++ 18_Fragments/Lerning-Fragments/src/App.jsx
@@ -4,11 +4,14 @@ import './App.css';
 function App() {
     
   let foodItems =  ['Dal', 'Fruits', 'Green Veg', 'Milk', 'Juices', 'Dry Fruits'];
+  let emptymsg = 
+  foodItems.length === 0 ? <h3>I am still hungry.</h3>: null; 
 
 return  (
   <center>
     <>
        <h1>Healthy Food</h1>
+       {emptymsg}
   <ul className="list-group">
   {foodItems.map((item) =>(
     <li key={item} className="list-group-item">{item}</li>
