d5d79b05a0f99525a47c5af0cf7eab5097e484aa
CrysPy-SAM <ssmishra9918@gmail.com>
2025-09-18 00:09:22 +0530

    Add main components and context for post management; implement CreatePost, PostList, and Post components

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -1,17 +1,35 @@
-import './App.css'
-import Header from './components/header';
-import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
+import PostListProvider from "./store/post-list-store";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
-    <>
-      <Sidebar></Sidebar>
-      <Header></Header>
-      <Footer></Footer>
-    </>
-  )
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
+          <Footer></Footer>
+        </div>
+      </div>
+    </PostListProvider>
+  );
 }
 
-export default App
+export default App;
