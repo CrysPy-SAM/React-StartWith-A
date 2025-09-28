7d19d3c2e2f9cdd4b782a117d2dac536b24f3082
CrysPy-SAM <ssmishra9918@gmail.com>
2025-09-28 22:00:32 +0530

    chnages

--- 46-react-router/src/components/WelcomeMessage.jsx
+++ 46-react-router/src/components/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 const WelcomeMessage = ({ onGetPostsClick }) => {
   return (
     <center className="welcome-message">
-      <h1>There are no posts</h1>
+      <h1>There are no posts.</h1>
     </center>
   );
 };
